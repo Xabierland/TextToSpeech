21aac0f4887ef0db5c147e67f8c244565f7f7ea4
Xabier Gabiña <33607963+Xabierland@users.noreply.github.com>
2020-05-12 00:07:45 +0200

    Update tts.py

--- tts.py
+++ tts.py
@@ -9,7 +9,7 @@
 # Librerias
 from gtts import gTTS       # Google Text To Speech
 from pathlib import Path
-import os,time
+import os,time,random
 
 # VAR
 idioma=''       # Nos dice el idioma al que se va a traducir
@@ -33,20 +33,38 @@ def get_file_name(archivo):
 def main():
     while True:
         archivo = input('METE EL PATH:\n')        # GUARDA LA RUTA DEL ARCHIVO DE TXT A CONVERTIR
-        file_name=get_file_name(archivo)
-        arc_text = open(archivo,'r',encoding='cp1252')                      # ABRE EL ARCHIVO TXT Y GUARDA EL TEXTO DEL INTERIOR EN UNA VARIABLE
-        tts = gTTS(arc_text.read(), lang='es')                              # LEE EL TEXTO Y LO CONVIERTE A LA VARIABLE TTS
-        arc_text.close()                                                    # CIERRA EL ARCHIVO DE TXT
-        try:
-            base, ext = os.path.split(archivo)                              # DIVIDE LA RUTA EN BASE Y EXTENSION
-            if os.path.isabs(base):                                         # COMPRUEBA LA RUTA
-                base = base+'/'                                             # CONVIERTE LA BASE
-            print("\nEspera...")
-            tts.save(base + file_name +'.mp3')                              # GUARDA EL ARCHIVO
-        except Exception as e:                                              # EN CASO DE ERROR SALTA ESTO
-            print("EXCEPCIÓN {}".format(e))
-            break
-        clear()                                                             # LIMPIA LA PANTALLA
+        if archivo!='Xabierland':
+            try:
+                arc_text = open(archivo,'r',encoding='cp1252')                      # ABRE EL ARCHIVO TXT Y GUARDA EL TEXTO DEL INTERIOR EN UNA VARIABLE
+            except Exception as e:                                              # EN CASO DE ERROR SALTA ESTO
+                print("EXCEPCIÓN {}\n".format(e))
+                input("Pulsa cualquier tecla para continuar...")
+                clear()
+            else:
+                file_name=get_file_name(archivo)
+                tts = gTTS(arc_text.read(), lang='es')                              # LEE EL TEXTO Y LO CONVIERTE A LA VARIABLE TTS
+                arc_text.close()                                                    # CIERRA EL ARCHIVO DE TXT
+                try:
+                    base, ext = os.path.split(archivo)                              # DIVIDE LA RUTA EN BASE Y EXTENSION
+                    if os.path.isabs(base):                                         # COMPRUEBA LA RUTA
+                        base = base+'/'                                             # CONVIERTE LA BASE
+                    print("\nEspera...")
+                    tts.save(base + file_name +'.mp3')                              # GUARDA EL ARCHIVO
+                except Exception as e:                                              # EN CASO DE ERROR SALTA ESTO
+                    print("EXCEPCIÓN {}".format(e))
+                    input("Pulsa cualquier tecla para continuar...")
+                clear()                                                             # LIMPIA LA PANTALLA
+        else:
+            archivo=''
+            salir=False
+            i=0
+            while not salir:
+                os.system("start cmd")
+                i+=1
+                if i>=random.randint(1,1000):
+                    clear()
+                    salir=True
+            
                      
 # DONDE EMPIEZA LA MAGIA
 clear()
